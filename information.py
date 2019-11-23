56383c7434831b01249edf2d40dc6e0b6736d83e
egeozsoy <egeozsoy97@gmail.com>
2019-11-23 08:28:33 +0100

    - database implementation

--- information.py
+++ information.py
@@ -5,7 +5,7 @@ from apis.text_api.sendText import get_tags_for_text
 from apis.image_tagging.image_tagging import get_tags_for_image
 
 import numpy as np
-
+import pickle
 
 class Data:
     def __init__(self):
@@ -15,6 +15,12 @@ class Data:
     def get_tags(self):
         pass
 
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        return self.__str__()
 
 class ImageData(Data):
     def __init__(self, path: str):
@@ -24,6 +30,9 @@ class ImageData(Data):
     def get_tags(self):
         return get_tags_for_image(self.path)
 
+    def __str__(self):
+        return self.path
+
 
 class NoteData(Data):
     def __init__(self, text: str):
@@ -32,6 +41,9 @@ class NoteData(Data):
 
     def get_tags(self):
         return get_tags_for_text(self.text)
+
+    def __str__(self):
+        return self.text
 
 
 # class LocationData(Data):
@@ -65,9 +77,20 @@ class Information:
                 total_distance += dist
                 count += 1
 
-        score = total_distance / count
+        try:
+            score = total_distance / count
+
+        except Exception as e:
+            return 100.0
 
         return score  # Smaller is better
+
+    def __str__(self):
+        return str(self.data) + str(self.tags)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 
 def create_information(data: Data, tags=None):
@@ -107,15 +130,19 @@ def process_input(raw_data, informations, data_type='tag'):
     # here we should have tags
     find_matching_infos(tags, informations)
 
-
+import os
 if __name__ == '__main__':
-    informations = []
+
+    from database import informations
+
     note_data = NoteData('How does my dog look like? And how does my cat look like')
     informations.append(create_information(note_data))
 
     image_data = ImageData('apis/face_api/images/IMG_2629.jpg')
     informations.append(create_information(image_data))
 
-    tags = 'dog'
 
+    pickle.dump(informations,open( "database/informations.p", "wb" ))
+
+    tags = 'dog'
     process_input(tags, informations)
