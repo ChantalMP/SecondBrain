703a3d79f86fe0687071aaff5fb00fe433da4f2f
michel.pessel <ga94tow@mytum.de>
2019-11-23 01:19:04 +0100

    sendText api implemented

---
+++ text_api/sendText.py
@@ -0,0 +1,8 @@
+from helpers.utils import send_json_api_requests
+
+url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.1/keyPhrases"
+
+payload = "{\n  \"documents\": [\n    {\n      \"language\": \"en\",\n      \"id\": \"1\",\n      \"text\": \"How does my dog look like?\"\n    }\n  ]\n}"
+
+send_json_api_requests(url,{},'d77dd9995c9e4714b4a29e6fd124a91d',payload)
+
